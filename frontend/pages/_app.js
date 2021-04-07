4604e339c2169c2b349dbb85e817365191308746
mohit <mohit.dhiman.sarkaghat@gmail.com>
2021-04-07 16:57:09 +0530

    Add to register page

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,7 @@
 import React from "react";
 import App from "next/app";
 import Head from "next/head";
-
+import Layout from "../components/Layout";
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
@@ -15,8 +15,9 @@ export default class MyApp extends App {
             crossOrigin="anonymous"
           />
         </Head>
-
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </>
     );
   }
