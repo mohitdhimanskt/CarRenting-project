0af32dbac7846812adea06a2016416ef7f308346
mohit <mohit.dhiman.sarkaghat@gmail.com>
2021-04-07 17:29:40 +0530

    Add to backend strapi

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8cb393bdd7a87584d24b528f08bb4197'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7398979b-c736-4c53-a864-4cd33eb35022'
+};
