48ec7af6691d7ea24c1170a1e41aea5a8a502cbd
mohit <mohit.dhiman.sarkaghat@gmail.com>
2021-04-29 18:34:30 +0530

    changes

--- frontend/components/checkout/CheckoutForm.js
+++ frontend/components/checkout/CheckoutForm.js
@@ -9,7 +9,6 @@ import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import CardSection from "./CardSection";
 import AppContext from "../../context/AppContext";
 
-
 function CheckoutForm() {
   const [data, setData] = useState({
     address: "",
